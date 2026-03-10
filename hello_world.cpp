60520f4361747228f9a62f61a77e52d16480f06a
from1k <ashubin2007@gmail.com>
2026-03-10 12:27:12 +0300

    new style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
 
-//Programm asks username and print hello world from @user.
-int main() {
-   std::sting name;
-   std::cout << "Enter your name: ";
-   std::cin >> name;
-   std::cout << "Hello world from " << name << std::endl;
-   return 0;
+// Programm asks username and print message
+int
+main()
+{
+  std::sting name;
+  std::cout << "Enter your name: ";
+  std::cin >> name;
+  std::cout << "Hello world from " << name << std::endl;
+  return 0;
 }
-
